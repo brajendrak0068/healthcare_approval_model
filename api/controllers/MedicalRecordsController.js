5680bb360b4bd68a1eb6cc0835f4fa32d1b7018c
brajendrak0068 <brajendrak0068@gmail.com>
2017-11-13 22:44:19 +0530

    Update MedicalRecordsController.js

--- api/controllers/MedicalRecordsController.js
+++ api/controllers/MedicalRecordsController.js
@@ -9,13 +9,11 @@
 module.exports = {
   show: function(req, res, next) {
     MedicalRecord.find({owner: req.params['userId']}).populate('prescriptions').populate('owner').exec(function (error, records){
-      console.log('recordsrecords', records);
-
       if(error){
         return next(error);
       }
       if (!records) {
-        return res.notFound('Could not find Finn, sorry.');
+        return res.notFound('Could not find records, sorry.');
       }
       res.view('medicalrecords/index', {records: records} )
     });
